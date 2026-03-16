2633c65fdb74d93fdbf3065c931b8cb1b3422edd
gabrielgustavo <gustavin@gmail.com>
2026-03-16 08:13:39 -0300

    Criação de classe Livro

---
+++ src/Livro.java
@@ -0,0 +1,13 @@
+import java.time.LocalTime;
+
+public class Livro {
+    ///tipo nomeVariavel;
+    String titulo;
+    String autor;
+    String narrador;
+    String editora;
+    LocalTime duracao;
+    String versao;
+    double preco;
+    String resumo;
+}
